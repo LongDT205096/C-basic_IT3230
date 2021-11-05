f5b1a516dd1b3c665fba3f65c2a7de9f3d4e35a1
LongDT205096 <91714440+LongDT205096@users.noreply.github.com>
2021-11-06 00:34:05 +0700

    Update integers.c

--- Week_5/integers.c
+++ Week_5/integers.c
@@ -9,14 +9,13 @@ struct Q_node{
     int front, rear;
 };
 
-
 typedef struct Q_node *queue;
 
 queue create_queue(int max_size){
     queue p = (struct Q_node*)malloc(sizeof(struct Q_node));
     p->max_size = max_size;
-    p->rear = -1;
-    p->front = -1;
+    p->rear = 0;
+    p->front = 0;
     p->data = (int*)malloc(max_size*sizeof(int));
     return p;
 }
@@ -31,44 +30,65 @@ int is_full(queue Q){
 
 void enQueue(queue Q, int data){
     if(is_full(Q)){
+        printf("Queue full\n");
         return;
+    } else{
+        Q->data[Q->rear] = data;
+        Q->rear = (Q->rear+1) % Q->max_size;
+        
+        printf("%d enqueued to queue %d\n",data,Q->rear);
     }
-    Q->rear = (Q->rear+1) % Q->max_size;
-    Q->data[Q->rear] = data;
-    printf("%d enqueued to queue %d\n",data,Q->rear);
 }
 
 void deQueue(queue Q){
     if(is_empty(Q)){
+        printf("Queue empty\n");
         return;
+    } else{
+        Q->front = (Q->front+1) % Q->max_size;
+        printf("%d\n",Q->front);
     }
-    Q->front = (Q->front+1)%Q->max_size;
-    int a = Q->data[Q->front];
-    Q->data[Q->front] = 0;
-    printf("%d dequeued from queue %d\n",a,Q->front);
 }
 
 void displayQueue(queue Q){
-    if (Q->front == -1){
-        printf("\nQueue is Empty");
+    if (Q->rear == Q->front){
+        printf("Queue is Empty\n");
         return;
     }
-    printf("\nElements in Circular Queue are: ");
 
-    if (Q->rear >= Q->front){
-        for (int i = Q->front; i <= Q->rear; i++)
+    printf("Elements in Circular Queue are: ");
+
+    if (Q->rear > Q->front){
+        for (int i = Q->front; i < Q->rear; i++)
             printf("%d ",Q->data[i]);
-    } else{
-        for (int i = 0; i <= Q->rear; i++)
-            printf("%d ", Q->data[i]);
+    } 
+    else if(Q->rear < Q->front){
+        for (int i = Q->front; i < Q->max_size; i++){
+            printf("%d ",Q->data[i]);
+        }
+
+        for(int i = 0; i < Q->rear; i++){
+            printf("%d ",Q->data[i]);
+        }
     }
+    printf("\n\n");
 }
 
 int main(){
-    int count=0;
-    queue Q = create_queue(10);
-    enQueue(Q,10);
-    enQueue(Q,20);
-    deQueue(Q);  
-    displayQueue(Q);  
+    int n;
+    queue Q = create_queue(6);
+    for(int i=1; i<=5; i++){
+        scanf("%d",&n);
+        enQueue(Q,n);
+    }
+    displayQueue(Q);
+    
+    deQueue(Q);
+    enQueue(Q,6);
+    displayQueue(Q);
+
+    deQueue(Q);
+    enQueue(Q,7);
+    displayQueue(Q);
+
 }
