5a6106218291c522695b48f4d8399e22dabf65a1
LongDT205096 <91714440+LongDT205096@users.noreply.github.com>
2021-12-17 01:28:10 +0700

    Update test.c

--- Week_4/test.c
+++ Week_4/test.c
@@ -8,6 +8,10 @@ int main() {
     in = fopen("kiphapbalan.txt","r");
     char a;
     stack_t root = create(100);
+    stack_t result = create(100);
+    stack_t math = create(100);
+    int num1, num2;
+
     while((a=fgetc(in)) != EOF){
         if(a == ')'){
             while(!is_empty(root)){
@@ -15,8 +19,33 @@ int main() {
                 pop(root);
             }
         }
-        if(a == '+' || a == '*') push(root,a);
-        if(a >= '0' && a <= '9') printf("%c ",a);
+        if(a == '+' || a == '*' || a == '-' || a == '/'){
+            push(root,a);
+            push(math,a);
+        }
+        if(a >= '0' && a <= '9'){
+            printf("%c ",a); 
+            push(result,a-48);
+        }     
     }
+
+    int sum;
+
+    while(!is_empty(math)){
+        char b = (char)top(math);
+        pop(math);
+        int a2 = (int)top(result);
+        pop(result);
+        sum = (int)top(result);
+        pop(result);
+        if(b == '+') sum += a2;
+        if(b == '-') sum -= a2;
+        if(b == '*') sum *= a2;
+        if(b == '/') sum /= a2;
+        push(result,sum);
+    }
+    printf("\n%d",sum);
+//đẩy dấu và số vào 2 stack riêng, lấy từng phần tử ra để thực hiện phép tính
+    
     fclose(in);
-}  
+}
